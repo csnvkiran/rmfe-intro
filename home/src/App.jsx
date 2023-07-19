65bfb8129b17ea009f98a06df156619921b469ba
csnvkiran <csnvkiran@gmail.com>
2023-07-19 18:54:22 +0400

    details page in mfea

--- home/src/App.jsx
+++ home/src/App.jsx
@@ -4,16 +4,13 @@ import ReactDOM from "react-dom";
 import "./index.scss";
 import Header from "./Header";
 import Footer from "./Footer";
+import HomeContent from "./HomeContent";
 
 const App = () => (
   <div className="text-3xl mx-auto max-w-6xl">
     <Header />
     <div className="mt-10">
-      Home Page Content
-      <div>Name: home</div>
-      <div>Framework: react</div>
-      <div>Language: JavaScript</div>
-      <div>CSS: Tailwind</div>
+      <HomeContent />
     </div>
     <Footer />
   </div>
--- mfea/src/App.jsx
+++ mfea/src/App.jsx
@@ -1,26 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Switch, Routes } from "react-router-dom";
 
 import "./index.scss";
+
 //const Header = React.lazy(() => import("home/Header"));
 import Header from "home/Header";
 import Footer from "home/Footer";
-import SafeComponent from "./SafeComponent";
+import MfeaContent from "./mfeaContent";
 
 const App = () => {
-  //const [showHeader, setShowHeader] = useState(false);
   return (
-    <div className="text-3xl mx-auto max-w-6xl">
-      <Header />
-      <div className="mt-10">
-        micro frontend Page Content
-        <div>Name: mfea</div>
-        <div>Framework: react</div>
-        <div>Language: JavaScript</div>
-        <div>CSS: Tailwind</div>
+    <BrowserRouter>
+      <div className="text-3xl mx-auto max-w-6xl">
+        <Header />
+        <div className="my-10">
+          <Routes>
+            <Route path="/product/:id" element={<MfeaContent />} />
+          </Routes>
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </BrowserRouter>
   );
 };
 
